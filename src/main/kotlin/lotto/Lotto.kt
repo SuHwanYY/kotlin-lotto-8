8d8836f278cd59703d6ecd4fb84f4a2cdcd73590
tnghk <tnghks2100@naver.com>
2025-11-03 18:20:00 +0900

    fix: Lotto 생성자에 중복, 범위 검증 추가

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,6 +3,8 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] 로또 번호는 6개여야 합니다." }
+        require(numbers.toSet().size == 6) { "[ERROR] 로또 번호는 중복될 수 없습니다." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다." }
     }
 
     // 외부에서 읽을 수 있도록 방어적 복사로 반환
